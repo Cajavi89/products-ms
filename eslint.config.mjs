9621b50f3501e328b9cd3a28146fe9362a4b5968
Carlos Jaramillo <ajaramillo@magnetoglobal.com>
2026-02-13 01:18:29 -0500

    actualizando proyecto

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,8 @@
 // @ts-check
-import eslint from '@eslint/js';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
-import globals from 'globals';
-import tseslint from 'typescript-eslint';
+import eslint from '@eslint/js'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
   {
@@ -17,7 +17,7 @@ export default tseslint.config(
         ...globals.node,
         ...globals.jest,
       },
-      sourceType: 'commonjs',
+      sourceType: 'module',
       parserOptions: {
         projectService: true,
         tsconfigRootDir: import.meta.dirname,
@@ -26,10 +26,12 @@ export default tseslint.config(
   },
   {
     rules: {
+      semi: 'off',
+      '@typescript-eslint/semi': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn',
-      "prettier/prettier": ["error", { endOfLine: "auto" }],
+      'prettier/prettier': ['error', { endOfLine: 'auto', semi: false }],
     },
   },
-);
+)
