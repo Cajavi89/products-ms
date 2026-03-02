42a454e52fac7dd9a85d17a36afe35c83cefa763
Carlos Jaramillo <ajaramillo@magnetoglobal.com>
2026-03-02 16:27:56 -0500

    implementacion comunicacion con NATS

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -4,15 +4,22 @@ import * as joi from 'joi'
 
 interface EnvVars {
   PORT: number
+
+  NATS_SERVERS: string[]
 }
 
 const envsSchema = joi
   .object<EnvVars>({
     PORT: joi.number().default(3001),
+
+    NATS_SERVERS: joi.array().items(joi.string()).required(),
   })
   .unknown(true)
 
-const { error, value } = envsSchema.validate(process.env)
+const { error, value } = envsSchema.validate({
+  ...process.env,
+  NATS_SERVERS: process.env.NATS_SERVERS?.split(','),
+})
 
 if (error) {
   throw new Error(`Config validation error: ${error.message}`)
@@ -22,4 +29,5 @@ const envVars: EnvVars = value
 
 export const envs = {
   PORT: envVars.PORT,
+  natsServers: envVars.NATS_SERVERS,
 }
